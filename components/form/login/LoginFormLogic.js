db811d394289cb8f6a14949fce3a1d5c095fb6b9
Karol Joniak <karol.joniak@visionect.io>
2021-05-30 10:42:55 +0200

    LoginFormLogic / fetch

--- components/form/login/LoginFormLogic.js
+++ components/form/login/LoginFormLogic.js
@@ -1,8 +1,9 @@
 import {useState} from "react";
+import {useRouter} from "next/router";
 
 const LoginFormLogic = () => {
 
-
+    const router = useRouter()
     const [state, setState] = useState({
         login: '',
         password: ''
@@ -16,9 +17,26 @@ const LoginFormLogic = () => {
         })
     }
 
-    const onSubmit = e => {
+    const onSubmit = async (e) => {
         e.preventDefault();
-        console.log(state);
+        const res = await fetch('http://localhost:5000/user/login', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                username: state.login,
+                password: state.password
+            })
+
+        })
+        console.log(res)
+        const res_parsed = await res.json()
+        console.log(res_parsed)
+        if(res_parsed.status === 200){
+            localStorage.setItem('token', res_parsed.data.token)
+            await router.push('/')
+        }
     }
 
     return {setData, onSubmit}
